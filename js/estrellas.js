695bbd315da1c6abb52b4f2d1617fbf8fb73b4da
kevinamunoz <kevinamunoz11@gmail.com>
2023-11-29 16:44:38 -0500

    estrellA

--- js/estrellas.js
+++ js/estrellas.js
@@ -1,174 +1,30 @@
 
-/* estrellas */
-let start1 = document.getElementById("start1")
-let start2 = document.getElementById("start2")
-let start3 = document.getElementById("start3")
-let start4 = document.getElementById("start4")
-let start5 = document.getElementById("start5")
-let puntuacion = document.getElementById("puntuacion").innerText
-    
-    if(puntuacion >= 0 && puntuacion <= 0.2) {
-        start1.setAttribute("class", "fa-regular fa-star")
-        start2.setAttribute("class", "fa-regular fa-star")
-        start3.setAttribute("class", "fa-regular fa-star")
-        start4.setAttribute("class", "fa-regular fa-star")
-        start5.setAttribute("class", "fa-regular fa-star")
-    }
-    else if(puntuacion >= 0.3 && puntuacion <= 0.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start2.setAttribute("class", "estrella fa-regular fa-star")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 0.8 && puntuacion <= 1.2) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-regular fa-star")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 1.3 && puntuacion <= 1.7){
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 1.8 && puntuacion <= 2.2) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 2.3 && puntuacion <= 2.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 2.8 && puntuacion <= 3.2){
-        start1.setAttribute("class", "fa-solid fa-star")
-        start2.setAttribute("class", "fa-solid fa-star")
-        start3.setAttribute("class", "fa-solid fa-star")
-        start4.setAttribute("class", "fa-regular fa-star")
-        start5.setAttribute("class", "fa-regular fa-star")
-    }
-    else if(puntuacion >= 3.3 && puntuacion <= 3.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 3.8 && puntuacion <= 4.2) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 4.3 && puntuacion <= 4.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star")
-        start5.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-    }
-    else if(puntuacion >= 4.8 && puntuacion <= 5){
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star")
-        start5.setAttribute("class", "estrella fa-solid fa-star")
-    }
-    /* otroo */
+document.addEventListener("DOMContentLoaded", function() {
+    const inputs = document.querySelectorAll("input");
+    const barras = document.querySelectorAll(".barra");
+    const porcentajes = document.querySelectorAll(".porcentaje");
+    const graficarBtn = document.getElementById("graficarBtn");
 
-    
-/* estrellas */
-let starts1 = document.getElementById("start1")
-let starts2 = document.getElementById("start2")
-let starts3 = document.getElementById("start3")
-let starts4 = document.getElementById("start4")
-let starts5 = document.getElementById("start5")
-let puntuacion2 = document.getElementById("puntuacion2").innerText
-    
-    if(puntuacion >= 0 && puntuacion <= 0.2) {
-        start1.setAttribute("class", "fa-regular fa-star")
-        start2.setAttribute("class", "fa-regular fa-star")
-        start3.setAttribute("class", "fa-regular fa-star")
-        start4.setAttribute("class", "fa-regular fa-star")
-        start5.setAttribute("class", "fa-regular fa-star")
-    }
-    else if(puntuacion >= 0.3 && puntuacion <= 0.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start2.setAttribute("class", "estrella fa-regular fa-star")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 0.8 && puntuacion <= 1.2) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-regular fa-star")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 1.3 && puntuacion <= 1.7){
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 1.8 && puntuacion <= 2.2) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-regular fa-star")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 2.3 && puntuacion <= 2.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start4.setAttribute("class", "estrella fa-regular fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 2.8 && puntuacion <= 3.2){
-        start1.setAttribute("class", "fa-solid fa-star")
-        start2.setAttribute("class", "fa-solid fa-star")
-        start3.setAttribute("class", "fa-solid fa-star")
-        start4.setAttribute("class", "fa-regular fa-star")
-        start5.setAttribute("class", "fa-regular fa-star")
-    }
-    else if(puntuacion >= 3.3 && puntuacion <= 3.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 3.8 && puntuacion <= 4.2) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star")
-        start5.setAttribute("class", "estrella fa-regular fa-star")
-    }
-    else if(puntuacion >= 4.3 && puntuacion <= 4.7) {
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star")
-        start5.setAttribute("class", "estrella fa-solid fa-star-half-stroke")
-    }
-    else if(puntuacion >= 4.8 && puntuacion <= 5){
-        start1.setAttribute("class", "estrella fa-solid fa-star")
-        start2.setAttribute("class", "estrella fa-solid fa-star")
-        start3.setAttribute("class", "estrella fa-solid fa-star")
-        start4.setAttribute("class", "estrella fa-solid fa-star")
-        start5.setAttribute("class", "estrella fa-solid fa-star")
-    }
+    graficarBtn.addEventListener("click", function() {
+        let totalVotos = 0;
+
+        // Obtener el total de votos
+        inputs.forEach((input) => {
+            const votos = parseFloat(input.value) || 0;
+            totalVotos += votos;
+        });
+
+        // Simulamos el llenado gradual de las barras al hacer clic en el botón
+        barras.forEach((barra, index) => {
+            const puntaje = parseFloat(inputs[index].value) || 0;
+
+            // Calcular la proporción y establecer el ancho de la barra
+            const proporcion = totalVotos > 0 ? (puntaje / totalVotos) * 100 : 0;
+            const anchoBarra = proporcion + "%";
+
+            // Actualizar la barra y el porcentaje
+            barra.style.width = anchoBarra;
+            porcentajes[index].textContent = proporcion.toFixed(2) + "%";
+        });
+    });
+});
